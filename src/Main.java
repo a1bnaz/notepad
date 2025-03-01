37e63acc57b4d36e438a678991bc4ec764c55568
albert <ieatbaconhair@gmail.com>
2025-02-28 22:18:26 -0500

    added cool emoji

--- src/Main.java
+++ src/Main.java
@@ -41,7 +41,7 @@ public class Main {
                 noteTaker.writeToFile(newFile, scanner);
             } else if (userChoice != 3) {
                 if (userChoice == 4) {
-                    System.out.println("Thank you for using the app 😎!");
+                    System.out.println("Thank you for using the app 😎👋!");
                     return;
                 }
             } else {
